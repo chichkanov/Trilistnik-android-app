4ad1b78f91acd0fb7daa416b823b6043e9ba7912
chichkanov <agchichkanov@gmail.com>
2017-03-12 19:39:24 +0300

    adding vk login

--- Trilistnik/Activities/Login.cs
+++ Trilistnik/Activities/Login.cs
@@ -6,12 +6,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Auth;
 
 namespace Trilistnik
 {
 	[Activity(Label = "Трилистник", MainLauncher = true)]
 	public class LoginActivity : Activity
 	{
+		public string token;
+		public string userId;
+
 		protected override void OnCreate(Bundle bundle)
 		{
 			SetTheme(Resource.Style.mainAppTheme);
@@ -22,16 +26,41 @@ namespace Trilistnik
 			Button skipLogin = FindViewById<Button>(Resource.Id.skipLogin);
 			Intent intent = new Intent(this, typeof(MainActivity));
 
-			login.Click += (object sender, EventArgs e) =>
-			{
-				Toast.MakeText(this, "Login Button Clicked!", ToastLength.Short).Show();
-			};
+			login.Click += delegate { LoginToVk(); };
 
 			skipLogin.Click += (object sender, EventArgs e) =>
 			{
 				Finish();
 				StartActivity(intent);
 			};
+		}
+
+		private void LoginToVk()
+		{
+			var auth = new OAuth2Authenticator(
+				clientId: "5920615",
+				scope: "friends,video,groups",
+				authorizeUrl: new Uri("https://oauth.vk.com/authorize"),
+				redirectUrl: new Uri("https://oauth.vk.com/blank.html"));
+			auth.AllowCancel = true;
+			auth.Completed += (sender, ee) =>
+			{
+				if (!ee.IsAuthenticated)
+				{
+					var builder = new AlertDialog.Builder(this);
+					builder.SetMessage("Not Authenticated");
+					builder.SetPositiveButton("Ok", (o, e) => { });
+					builder.Create().Show();
+					return;
+				}
+				else
+				{
+					token = ee.Account.Properties["access_token"].ToString();
+					userId = ee.Account.Properties["user_id"].ToString();
+				}
+			};
+			var intent = auth.GetUI(this);
+			//StartActivity(intent);
 		}
 	}
 }
